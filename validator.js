e3271413a33c507360baff562819b95f966f458f
lts <lts@1ui-MacBook-Air.local>
2022-07-31 06:39:29 +0700

    31/7

--- validator.js
+++ validator.js
@@ -1,4 +1,9 @@
-function Validator(formSelector){
+function Validator(formSelector, options){
+    // Gán giá trị mặc định cho tham số khi định nghĩa (ES5)
+    if(!options) {
+        options = {}
+    }
+
     function getParent(element,selector) {
         while (element.parentElement) {
             if (element.parentElement.matches(selector)) {
@@ -110,7 +115,7 @@ function Validator(formSelector){
             var rules = formRules[event.target.name];
             var errorMessage
             for (var i in rules) {
-                console.log(rules)  
+                // console.log(rules)  
                 errorMessage = rules[i](event.target.value)
             if (errorMessage) break;
             }
@@ -124,8 +129,10 @@ function Validator(formSelector){
                         formMessage.innerText = errorMessage
                     }
                 }
-                console.log(formGroup)
             }
+            // nếu errorMessage = undefined thì !undefined là true
+            return !errorMessage
+            
         }
         console.log(formRules)
 
@@ -139,6 +146,60 @@ function Validator(formSelector){
                     if (formMessage) {
                         formMessage.innerText = ''
                     }
+            }
+        }
+    }
+
+    // Xử lý hành vi submit form
+    formElement.onsubmit = function (event) {
+        event.preventDefault()
+
+        var inputs = formElement.querySelectorAll('[name][rules]')
+        var isValid = true
+
+        for (var input of inputs){
+            if(!handleValidate({target: input})) {
+                isValid = false
+            }
+        }
+        
+        // Khi không có lỗi thì sumbit form
+        if (isValid) {
+            if(typeof options.onSubmit === 'function') {var enableInputs = formElement.querySelectorAll('[name]')
+            // console.log(Array.from(enableInputs))
+            // Array.from() convert thành 1 array, trả về các thẻ input chứa thuộc tính name
+            var formValues = Array.from(enableInputs).reduce(function(values,input){
+                switch(input.type) {
+                    case'radio':
+                    // checked là pseudo class
+                        values[input.name] = formElement.querySelector('input[name="' + input.name +  '"]:checked').value   
+                        break;                              
+                    case'checkbox':
+                        if(!input.matches(':checked') && !values[input.name]){
+                            values[input.name] = ''
+                        }
+                        if(!input.matches(':checked')){
+                            return values;
+                        } 
+                        if(!Array.isArray(values[input.name])) {
+                            values[input.name] = []
+                        }
+                        values[input.name].push(input.value)
+                        break
+                    case 'file':
+                        values[input.name] = input.files;
+                        break
+                    default:
+                        values[input.name] = input.value 
+                }
+                console.log(values)
+                return values
+            },{})    
+
+            // Gọi lại hàm onSubmit và trả về kèm giá trị của form
+            options.onSubmit(formValues)
+            } else {
+                formElement.submit()
             }
         }
     }
